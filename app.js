a6c8109c3eb228bb50661e7b4741864aaaf0a4b2
Deniss <den.b@itservicesgroup.co.uk>
2017-03-22 22:32:25 +0000

    Add Connect bootstrap template with app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 var app = express();
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'))
 app.get('/', function (req, res) {
   res.send("hello world");
 })
