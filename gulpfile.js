df3c12f1511ebe98738993d7945aa207ccb64422
Deniss <den.b@itservicesgroup.co.uk>
2017-03-23 17:17:31 +0000

    Add Task to restart server if any js files changes
    Used gulp-nodemon library
    $ gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jsFiles = ['*.js', 'src/**/*.js'];
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('style', function () {
   return gulp.src(jsFiles)
@@ -32,3 +33,18 @@ gulp.task('inject', function () {
     .pipe(inject(injectSrc, injectOptions))
     .pipe(gulp.dest('./src/views'));
 });
+
+gulp.task('serve', ['style', 'inject'], function () {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 3000
+    },
+    watch: jsFiles
+  };
+  return nodemon(options)
+    .on('restart', function (ev) {
+      console.log('Restarting ...');
+    })
+})
