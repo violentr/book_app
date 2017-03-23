840a73f3f179c04a6ad945ac6b4665d165bd3e3d
Deniss <den.b@itservicesgroup.co.uk>
2017-03-23 19:14:34 +0000

    Add handlebars template to index.hbs
    Made all necessary preparation to
    use handlebars as templating engine
    for rendering html

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require("express");
-
+var express = require('express');
 var app = express();
-var port = 5000;
-
+var port = process.env.PORT || 5000;
+var list = ['a', 'b']
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({
+  extname: '.hbs'
+}));
 app.use(express.static('public'));
-app.use(express.static('src/views'))
+app.set('views', './src/views');
+app.set('view engine', '.hbs');
 app.get('/', function (req, res) {
-  res.send("hello world");
-})
+  res.render('index', {
+    title: "hello from title",
+    list: list
+  });
+});
 
 app.listen(port, function (err) {
-  console.log('running server on port', port)
+  console.log('running server on port', port);
 });
