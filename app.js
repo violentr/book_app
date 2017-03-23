fd86b6a92656744314cc6ddecf0f5b6698fe3efa
Deniss <den.b@itservicesgroup.co.uk>
2017-03-23 21:02:10 +0000

    Add Rename index.html to index.ejs
    Connect ejs template to the app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var list = ['a', 'b']
+var list = ['a', 'b', 'c', 'd']
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({
   extname: '.hbs'
@@ -9,6 +9,7 @@ app.engine('.hbs', handlebars({
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('index', {
     title: "hello from title",
