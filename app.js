725e3e181826970a74e38a6686490f996a0ebfa5
Deniss <den.b@itservicesgroup.co.uk>
2017-04-17 14:39:28 +0100

    Add simple routing for Books, as example

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var list = ['a', 'b', 'c', 'd']
+var bookRouter = express.Router();
+var nav = [{
+  Link: '/Books',
+  Text: 'Books'
+}, {
+  Link: '/Authors',
+  Text: 'Authors'
+}]
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({
   extname: '.hbs'
@@ -12,10 +19,22 @@ app.set('view engine', '.hbs');
 app.set('view engine', '.jade');
 app.set('view engine', 'ejs');
 
+
+bookRouter.route('/')
+  .get(function (req, res) {
+    res.send("Hello Books");
+  });
+
+bookRouter.route('/single')
+  .get(function (req, res) {
+    res.send("Hello single book!");
+  });
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
   res.render('index', {
     title: "hello from title",
-    list: list
+    nav: nav
   });
 });
 
