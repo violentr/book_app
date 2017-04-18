07296abe3b68b58ce039537b6ae5a7dc32ca6fdc
Deniss <den.b@itservicesgroup.co.uk>
2017-04-18 15:04:21 +0100

    Add Show selected book from book list
    Retrieve data from mongodb, collection.findOne.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav, db) {
   bookRouter.route('/')
@@ -22,17 +23,21 @@ var router = function (nav, db) {
 
   bookRouter.route('/:id')
     .get(function (req, res) {
-      var id = req.params.id;
-      db.query('select * from books where book_id=?', id, function (err, recordset) {
-        if (recordset.length === 0) {
-          res.status(404).send('Book Not Found!');
-        } else {
-          res.render('bookView', {
-            nav: nav,
-            book: recordset[0]
-          });
-        }
+      var id = new objectId(req.params.id);
+      var url = 'mongodb://localhost:27017/booksApp';
+      mongodb.connect(url, function (err, db) {
+        var collection = db.collection('books');
+        collection.findOne({
+            _id: id
+          },
+          function (err, results) {
+            res.render('bookView', {
+              nav: nav,
+              book: results
+            });
 
+          }
+        );
       });
     });
   return bookRouter;
