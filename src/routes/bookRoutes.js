9c907420fd0a73da104cc38f63b58d13043c7828
Deniss <den.b@itservicesgroup.co.uk>
2017-04-18 12:16:50 +0100

    Add Handle Error when book is not found
    nothing special simple message "book not found"

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -16,12 +16,16 @@ var router = function (nav, db) {
 
   bookRouter.route('/:id')
     .get(function (req, res) {
-      var id = req.params.id
-      db.query("select * from books where book_id=?", id, function (err, recordset) {
-        res.render("bookView", {
-          nav: nav,
-          book: recordset[0]
-        });
+      var id = req.params.id;
+      db.query('select * from books where book_id=?', id, function (err, recordset) {
+        if (recordset === 0) {
+          res.status(404).send('Book Not Found!');
+        } else {
+          res.render('bookView', {
+            nav: nav,
+            book: recordset[0]
+          });
+        }
       });
     });
   return bookRouter;
