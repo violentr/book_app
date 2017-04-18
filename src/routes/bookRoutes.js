2df27610d55092bcfee8f95f4dd2b3f1c5de3ea9
Deniss <den.b@itservicesgroup.co.uk>
2017-04-18 12:02:28 +0100

    Add List books or ceartain book by calling db.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,28 +1,27 @@
 var express = require('express');
 var bookRouter = express.Router();
-var books = [];
 
 var router = function (nav, db) {
   db.connect();
   bookRouter.route('/')
     .get(function (req, res) {
       db.query("select * from books", function (err, recordset) {
-        books = recordset;
         console.log(recordset);
-      });
-      res.render("bookListView", {
-        nav: nav,
-        books: books
+        res.render("bookListView", {
+          nav: nav,
+          books: recordset
+        });
       });
     });
 
   bookRouter.route('/:id')
     .get(function (req, res) {
       var id = req.params.id
-      var currentItem = id - 1
-      res.render("bookView", {
-        nav: nav,
-        book: books[currentItem]
+      db.query("select * from books where book_id=?", id, function (err, recordset) {
+        res.render("bookView", {
+          nav: nav,
+          book: recordset[0]
+        });
       });
     });
   return bookRouter;
