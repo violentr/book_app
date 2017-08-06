7f39a7d712a9ac51d2d9a275b1f54ac1d4956bb4
Deniss <den.b@itservicesgroup.co.uk>
2017-08-06 16:40:59 +0100

    Add library passport-local, user auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,11 +4,29 @@ var mongodb = require('mongodb').MongoClient;
 
 
 var router = function () {
-  authRouter.route('/signUp')
+  authRouter.route('/signup')
     .post(function (req, res) {
       console.log(req.body);
+      var url = "mongodb://localhost:27017/booksApp";
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('users');
+        var user = {
+          username: req.body.username,
+          password: req.body.password
+        };
+      collection.insert(user, function(err, results){
+        req.login(results.ops[0], function(){
+          res.redirect('/auth/profile');
+
+        });
+      });
+      });
     });
+  authRouter.route('/profile')
+    .get(function(req, res){
+    res.json(req.user);
+  });
   return authRouter;
-}
+};
 
 module.exports = router;
